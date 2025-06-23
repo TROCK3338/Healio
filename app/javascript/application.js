f784c60cb5b590c1501cfed90a489ed705b1306e
Aman Singhal <trock3338@gmail.com>
2025-06-24 01:48:46 +0530

    Updates

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,5 @@
 import "@hotwired/turbo-rails"
 import "controllers"
-import "chartkick/chart.js"
 
-import Rails from "@rails/ujs" // only if you're using `method: :delete` in links
+import Rails from "@rails/ujs"
 Rails.start()
